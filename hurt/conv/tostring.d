60dfad8e48dd280e787ceea4f2a6d4b9a60c6851
Robert BuRnEr Schadek <rburners@gmail.com>
2011-02-10 00:52:51 +0100

    had another 3 min

--- hurt/conv/tostring.d
+++ hurt/conv/tostring.d
@@ -16,8 +16,6 @@ public pure immutable(T)[] intToString(T)(int src)
 			tmp[tmpptr++] = '\u002D'; // yes 002D means -
 		} else static if(is(T == dchar)) {
 			tmp[tmpptr++] = '\U0000002D'; // yes 0000002D means -
-		} else {
-			assert(0, "invalid case");
 		}
 	}
 	byte toConv;
@@ -39,19 +37,32 @@ public pure immutable(T)[] intToString(T)(int src)
 		return tmp[0..tmpptr].reverse.idup;
 }
 
-public pure string shortToString(short src) {
-	char[8] tmp;
+public pure immutable(T)[] shortToString(T)(short src)
+		if(is(T == char) || is(T == wchar) || is(T == dchar)) {
+	T[8] tmp;
 	uint tmpptr = 0;
 	bool sign = false;
 	if(src < 0) {
 		src = -src;
 		sign = true;
-		tmp[tmpptr++] = '-';
+		static if(is(T == char)) {
+			tmp[tmpptr++] = '-';
+		} else static if(is(T == wchar)) {
+			tmp[tmpptr++] = '\u002D'; // yes 002D means -
+		} else static if(is(T == dchar)) {
+			tmp[tmpptr++] = '\U0000002D'; // yes 0000002D means -
+		}
 	}
 	byte toConv;
 	while(src) {
 		toConv = cast(byte)(src % 10);	
-		tmp[tmpptr++] = byteToCharBase10!(char)(toConv);
+		static if(is(T == char)) {
+			tmp[tmpptr++] = byteToCharBase10!(char)(toConv);
+		} else static if(is(T == wchar)) {
+			tmp[tmpptr++] = byteToCharBase10!(wchar)(toConv);
+		} else static if(is(T == dchar)) {
+			tmp[tmpptr++] = byteToCharBase10!(dchar)(toConv);
+		}
 		src /= 10;
 	}
 	if(sign) {
