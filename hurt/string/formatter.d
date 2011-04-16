9f8ab12098c4848f5efde85e0c29546f8cbc1139
Robert BuRnEr Schadek <rburners@gmail.com>
2011-04-16 16:32:51 +0200

    some more and it builds

--- hurt/string/formatter.d
+++ hurt/string/formatter.d
@@ -10,14 +10,15 @@ enum ParseState {
 }
 
 public pure immutable(T)[] format(T,S)(immutable(S)[] format, ...) 
-		(if(is(T == char) || is(T == wchar) || is(T == dchar)) &&
-		(if(is(S == char) || is(S == wchar) || is(S == dchar)) {
+		if((is(T == char) || is(T == wchar) || is(T == dchar)) &&
+		(is(S == char) || is(S == wchar) || is(S == dchar))) {
 	size_t ptr = 0;
 	size_t vaTypePtr = 0;
 	T[] ret = new T[32];
-	foreach(idx, it, format) {
-		if(it != '%') {
-			appendWithIdx(ret, ptr++, it);
+	for(size_t idx; idx < format.length; idx++) {
+		// no special treatment till you find a % character
+		if(format[idx] != '%') {
+			appendWithIdx(ret, ptr++, format[idx]);
 		} else {
 			ParseState formState = ParseState.None;
 			
