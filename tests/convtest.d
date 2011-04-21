7a3bfe1c7424028b8680fd704ef89e8830b69cf1
Robert BuRnEr Schadek <rburners@gmail.com>
2011-04-21 14:31:39 +0200

    length and precision as vararg parameter works again

--- tests/convtest.d
+++ tests/convtest.d
@@ -1,5 +1,5 @@
 import hurt.conv.conv;
-import hurt.conv.numerictochar;
+import hurt.conv.tostring;
 
 import std.stdio;
 
@@ -9,10 +9,4 @@ void main() {
 	assert(42 == conv!(string, int)("__4__2__"));
 	assert("42" == conv!(int, string)(42));
 	assert("-42" == conv!(int, string)(-42));
-
-	assert("10" == integerToBase8!(int, char)(8), integerToBase8!(int,char)(8));
-	assert("-20" == integerToBase8!(byte, char)(-16), integerToBase8!(int,char)(-16));
-	assert("-22" == integerToBase8!(byte, char)(-18), integerToBase8!(int,char)(-18));
-	assert("-22"w == integerToBase8!(byte, wchar)(-18), integerToBase8!(int,char)(-18));
-	assert("-26"d == integerToBase8!(byte, dchar)(-22), integerToBase8!(int,char)(-22));
 }
