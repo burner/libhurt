0711349910b889c081527e94b10833457044d13b
Robert BuRnEr Schadek <rburners@gmail.com>
2011-06-16 11:26:42 +0200

    some stuff to build with the latest dmd

--- hurt/io/ioflags.d
+++ hurt/io/ioflags.d
@@ -1,24 +1,25 @@
 module hurt.io.ioflags;
+import std.conv;
 
 enum FileFlags {
 	O_ACCMODE =	 00000003,
 	O_RDONLY  =  00000000,
 	O_WRONLY  =  00000001,
 	O_RDWR	  =  00000002,
-	O_CREAT	  =  00000100,	/* not fcntl */
-	O_EXCL	  =  00000200,	/* not fcntl */
-	O_NOCTTY  =  00000400,	/* not fcntl */
-	O_TRUNC	  =  00001000,	/* not fcntl */
-	O_APPEND  =  00002000,
-	O_NONBLOCK = 00004000,
-	O_DSYNC	  =  00010000,	/* used to be O_SYNC, see below */
-	FASYNC	  =  00020000,	/* fcntl, for BSD compatibility */
-	O_DIRECT  =  00040000,	/* direct disk access hint */
-	O_LARGEFILE =00100000,
-	O_DIRECTORY= 00200000,	/* must be a directory */
-	O_NOFOLLOW=	 00400000,	/* don't follow links */
-	O_NOATIME =	 01000000,
-	O_CLOEXEC =	 02000000	/* set close_on_exec */
+	O_CREAT	  =  std.conv.octal!100,	/* not fcntl */
+	O_EXCL	  =  std.conv.octal!200,	/* not fcntl */
+	O_NOCTTY  =  std.conv.octal!400,	/* not fcntl */
+	O_TRUNC	  =  std.conv.octal!1000,	/* not fcntl */
+	O_APPEND  =  std.conv.octal!2000,
+	O_NONBLOCK = std.conv.octal!4000,
+	O_DSYNC	  =  std.conv.octal!10000,	/* used to be O_SYNC, see below */
+	FASYNC	  =  std.conv.octal!20000,	/* fcntl, for BSD compatibility */
+	O_DIRECT  =  std.conv.octal!40000,	/* direct disk access hint */
+	O_LARGEFILE =std.conv.octal!100000,
+	O_DIRECTORY= std.conv.octal!200000,	/* must be a directory */
+	O_NOFOLLOW=	 std.conv.octal!400000,	/* don't follow links */
+	O_NOATIME =	 std.conv.octal!1000000,
+	O_CLOEXEC =	 std.conv.octal!2000000	/* set close_on_exec */
 }
 
 enum ModeValues {
