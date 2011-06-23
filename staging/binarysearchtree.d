041c8be7cfc128f3a2791a37099e0d73118ba560
Robert BuRnEr Schadek <rburners@gmail.com>
2011-06-23 13:52:15 +0200

    some stuff works

---
+++ staging/binarysearchtree.d
@@ -0,0 +1,126 @@
+import std.stdio;
+
+class Node(T) {
+    // By value storage of the data
+    T data;
+ 
+    // Combine the two branches into an array to optimize the logic
+    Node!(T) link[2];
+	Node!(T) parent;
+ 
+    // Null the branches so we don't have to do it in the implementation
+    this(T data) {
+		this.data = data;
+		this.parent = null;
+        // Left branch
+        this.link[0] = null;
+ 
+        // Right branch
+        this.link[1] = null;
+    }
+
+/*	bool validate(bool root, const Node!(T) par = null) const {
+		if(!root) {
+			if(this.parent is null) {
+				writeln(__FILE__,__LINE__,": parent is null");
+				return false;
+			}
+			if(this.parent !is par) {
+				writeln(__FILE__,__LINE__,": parent is wrong ", parent.key, " ",par.key);
+				return false;
+			}
+		}
+		bool left = true;
+		bool right = true;
+		if(this.link[0] !is null) {
+			assert(this.link[0].parent is this);
+			left = this.link[0].validate(false, this);
+		}
+		if(this.link[1] !is null) {
+			assert(this.link[1].parent is this);
+			right = this.link[1].validate(false, this);
+		}
+		return left && right;
+	}
+
+	void print() const {
+		writeln(key);
+		if(this.link[0] !is null) {
+			this.link[0].print();
+		}
+		if(this.link[1] !is null) {
+			this.link[1].print();
+		}
+	}
+
+	public override int toHash() const {
+		return this.key;
+	}*/
+}
+
+class BinarySearchTree(T) {
+	private Node!(T) root;
+	
+	void insert(T data) {
+		if(this.root is null) {
+			this.root = new Node!(T)(data);
+			return;
+		}
+		Node!(T) node = this.search(data);
+		if(node is null) {
+			writeln(node);
+			node = new Node!(T)(data);
+		}
+	}
+
+	Node!(T) search(T data) {
+		Node!(T) node = this.root;
+		while(node !is null) {
+			if(data < node.data) {
+				node = node.link[0];
+			} else if(node.data < data) {
+				node = node.link[1];
+			} else {
+				break;
+			}
+		}
+		return node;
+	}
+
+	void remove(ref Node!(T) node) {
+		if(node is null)
+			return;
+		Node!(T) old = node;
+		if(node.link[0] is null) {
+			node = node.link[1];
+			old = null;
+		} else if(node.link[1] is null) {
+			writeln(__LINE__);
+			node = node.link[0];
+			old = null;
+		} else {
+			Node!(T) pred = node.link[0];
+			while(pred.link[1] !is null) {
+				pred = pred.link[1];
+			}
+			swap(pred, node);
+			remove(pred);
+		}
+	}
+
+	private void swap(Node!(T) a, Node!(T) b) {
+		T tmp = a.data;
+		a.data = b.data;
+		b.data = tmp;
+	}
+}
+
+void main() {
+	auto a = new BinarySearchTree!(int)();
+	a.insert(1);
+	assert(a.search(1));
+	Node!(int) af = a.search(1);
+	assert(af is a.root);
+	a.remove(af);
+	assert(af is null);
+}
