83873adc7348b1d88398c741766a75c73dbaede7
Robert BuRnEr Schadek <rburners@gmail.com>
2011-09-21 09:28:06 +0200

    rangemap coming good

--- staging/rangemap.d
+++ staging/rangemap.d
@@ -2,6 +2,8 @@ module hurt.container.rangemap;
 
 import hurt.container.multimap;
 import hurt.container.map;
+import hurt.algo.sorting;
+import hurt.io.stdio;
 
 private class Node(T,S) {
 	private T first;
@@ -15,6 +17,13 @@ private class Node(T,S) {
 		this.first = first;
 		this.value = value;
 		this.lastSet = false;
+	}
+
+	this(T first, T last, S value) {
+		this.first = first;
+		this.last = last;
+		this.value = value;
+		this.lastSet = true;
 	}
 
 	public bool canExpend(T next) const {
@@ -53,6 +62,14 @@ private class Node(T,S) {
 		return this.last;
 	}
 
+	public Node!(T,S) getLeft() {
+		return this.left;
+	}
+
+	public Node!(T,S) getRight() {
+		return this.right;
+	}
+
 	public S getValue() {
 		return this.value;
 	}
@@ -61,20 +78,22 @@ private class Node(T,S) {
 		return this.lastSet;
 	}
 
-	package void setNode(Node!(T,S) node) {
-		if(node.getFirst() < this.first && node.getLast < this.first) {
+	package bool setNode(Node!(T,S) node) {
+		if(node is null) {
+			return true;
+		} else if(node.isLastSet() && node.getLast() < this.first) {
 			this.left = node;
-		} else if(node.isLastSet() && this.lastSet && 
-				node.getFirst() > this.last) {
+		} else if(!node.isLastSet() && node.getFirst() < this.first) {
+			this.left = node;
+		} else if(this.lastSet && node.getFirst() > this.last) {
 			this.right = node;
-		} else if(node.isLastSet() && !this.lastSet && 
-				node.getFirst() > this.first) {
+		} else if(!this.lastSet && node.getFirst() > this.first) {
 			this.right = node;
 		} else {
-			assert(0, "node is not smaller nor bigger, thats wrong");
+			return false;
 		}
+		return true;
 	}
-
 }
 
 unittest {
@@ -84,9 +103,76 @@ unittest {
 	n.expend('b');
 	assert(n.canExpend('c'));
 	assert(!n.canExpend('b'));
+
+	Node!(dchar,int) m = new Node!(dchar,int)('h', 'i', 0);
+	Node!(dchar,int) i = new Node!(dchar,int)('a', 'b', 1);
+	Node!(dchar,int) j = new Node!(dchar,int)('m', 'o', 2);
+	assert(!m.setNode(m));
+	assert(!i.setNode(i));
+	assert(!j.setNode(j));
+	assert(m.setNode(i));
+	assert(m.setNode(j));
+	assert(m.getLeft() is i);
+	assert(m.getRight() is j);
+
+	Node!(dchar,int) m1 = new Node!(dchar,int)('h', 0);
+	Node!(dchar,int) i1 = new Node!(dchar,int)('a', 1);
+	Node!(dchar,int) j1 = new Node!(dchar,int)('m', 2);
+	assert(!m1.setNode(m1));
+	assert(!i1.setNode(i1));
+	assert(!j1.setNode(j1));
+	assert(m1.setNode(i1));
+	assert(m1.setNode(j1));
+	assert(m1.getLeft() is i1);
+	assert(m1.getRight() is j1);
 }
 
 class RangeMap(T,S) {
+	private Node!(T,S) root;
+
+	this() { }
+
+	this(Node!(T,S)[] nodes) {
+		if(nodes.length == 0)
+			return;
+
+		sort!(Node!(T,S))(nodes, 
+			function(in Node!(T,S) a, in Node!(T,S) b) {
+				return a.getFirst() < b.getFirst(); 
+			});
+
+		size_t middle = (0 + nodes.length)/2;
+		this.root = nodes[middle];
+		assert(this.root.setNode(recursiveTreeCreate(nodes, 0, middle)));
+		assert(this.root.setNode(recursiveTreeCreate(nodes, middle+1, 
+			nodes.length)));
+
+	}
+
+	private static Node!(T,S) recursiveTreeCreate(Node!(T,S)[] nodes, 
+			size_t left, size_t right) {
+		if(left >= right) {
+			return null;
+		}
+		size_t middle = (left + right)/2;
+		Node!(T,S) mid = nodes[middle];
+		assert(mid.setNode(recursiveTreeCreate(nodes, left, middle)));
+		assert(mid.setNode(recursiveTreeCreate(nodes, middle+1, right)));
+		return mid;
+	}
+}
+
+unittest {
+	Node!(dchar,int)[] ar1 = new Node!(dchar,int)[1];
+	ar1[0] = new Node!(dchar,int)('a',0);
+	RangeMap!(dchar,int) m1 = new RangeMap!(dchar,int)(ar1);
+	for(size_t i = 1; i < 26; i++) {
+		ar1 = new Node!(dchar,int)[i];
+		for(size_t j = 0; j < i; j++) {
+			ar1[j] = new Node!(dchar,int)(cast(dchar)(j+97), cast(int)j);
+		}
+		m1 = new RangeMap!(dchar,int)(ar1);
+	}
 }
 
 void main() {
