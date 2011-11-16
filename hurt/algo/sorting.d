054cdfae8c5ff2b5dcb6855612bbae1ea03c72ca
Robert BuRnEr Schadek <rburners@gmail.com>
2011-11-17 00:14:37 +0100

    sort for deque

--- hurt/algo/sorting.d
+++ hurt/algo/sorting.d
@@ -2,6 +2,7 @@ module hurt.algo.sorting;
 
 import hurt.conv.conv;
 import hurt.container.vector;
+import hurt.container.deque;
 
 /* This sort function implements a iterativ quicksort.
  * you can put any kind of container that implements the [] operator. If you 
@@ -176,5 +177,73 @@ unittest {
 		k.pushBack(it);
 	}
 	sortVector!(int)(k, function(in int l, in int r) { return l < r; });
+	test!(int)(k);
+}
+
+void sortDeque(T)(Deque!(T) a, bool function(in T a, in T b) cmp, 
+		size_t leftb = 0, size_t rightb= 0) {
+	debug assert(rightb <= a.getSize()-1, "right index out of bound");
+	debug assert(leftb <= rightb, "left index to big");
+
+	//swap function
+	void swap(size_t m, size_t n) {
+		T tmp = a[m];
+		//a.set(m, a[n]);
+		//a.set(n, tmp);
+		a[m] = a[n];
+		a[n] = tmp;
+	}
+
+	//partition function
+	long partition(size_t left, size_t right) {
+		size_t idx = (left+right+1)/2;
+		const T pivot = a[idx];
+		swap(idx, right);
+		for(ulong i = idx = left; i < right; i++) {
+			if(cmp(a[i], pivot)) {
+				swap(idx++, i);
+			}
+		}
+		swap(idx, right);
+		return idx;
+	}
+
+	//the actual quicksort begins here
+	long[128] stack;
+	long stackTop = 0;
+	stack[stackTop++] = leftb;
+	if(rightb != 0) {
+		stack[stackTop++] = rightb;
+	} else {
+		stack[stackTop++] = a.getSize()-1;
+	}
+	while(stackTop > 0) {
+		long right = stack[--stackTop];
+		long left = stack[--stackTop];
+		while(right > left) {
+			long i = partition(left, right);
+			if(i-1 > left) {
+				stack[stackTop++] = left;
+				stack[stackTop++] = i-1;
+			}
+			left = i+1;
+		}
+	}
+}
+
+unittest {
+	void test(T)(Deque!(T) v) {
+		foreach(idx, it; v) {
+			if(idx > 0) {
+				assert(v[idx-1] < it, 
+					conv!(size_t,string)(v[idx-1]) ~ " " ~ 
+					conv!(size_t,string)(it));
+			}
+		}
+	}
+
+	Deque!(int) k = new Deque!(int)([0, 31, 32, 1027, 1540, -1,1541, 1452, 1546,
+		1547, 1036, 1282, 526]);
+	sortDeque!(int)(k, function(in int l, in int r) { return l < r; });
 	test!(int)(k);
 }
