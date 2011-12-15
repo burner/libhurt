abf38e9b249888677b9be7fd5a5fdc8a1332a92c
Robert BuRnEr Schadek <rburners@gmail.com>
2011-12-15 23:04:04 +0100

    more on the mapset

--- staging/mapset.d
+++ staging/mapset.d
@@ -90,12 +90,14 @@ public class MapSet(T,S) {
 	public size_t getSize() {
 		size_t ret = 0;
 		ISRIterator!(MapItem!(T,Set!(S))) it = this.map.begin();
-		for(; it.isValid(); it++) {
-			ret += (*it).getData().getSize();
+		for(; it.isValid(); it.increment()) {
+			ret += it.getData().getData().getSize();
 		}
 		return ret;
 	}
 
+	/** Check if the set of map t contains s
+	 */
 	public bool contains(T t, S s) {
 		MapItem!(T,Set!(S)) mi = this.map.find(t);
 		if(mi !is null) {
@@ -104,25 +106,103 @@ public class MapSet(T,S) {
 			return false;
 		}
 	}
+
+	/** Check if any of the maps contains s.
+	 */
+	public bool containsElement(S s) {
+		ISRIterator!(MapItem!(T,Set!(S))) it = this.map.begin();
+		for(; it.isValid(); it.increment()) {
+			if(it.getData().getData().contains(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/** Check if there is a map by the key of t.
+	 */
+	public bool containsMapping(T t) {
+		MapItem!(T,Set!(S)) it = this.map.find(t);
+		return it !is null;
+	}
+
+	package Map!(T,Set!(S)) getMap() {
+		return this.map;
+	}
+}
+
+/* If I make this a member of the mapset the compiler complains about
+ * Error: template instance hurt.container.isr.ISRIterator!(MapItem).
+ * ISRIterator.opUnary!("++") recursive expansion
+ */
+public bool Equals(T,S)(MapSet!(T,S) ms, MapSet!(T,S) ns) {
+	ISRIterator!(MapItem!(T,Set!(S))) it = ns.getMap().begin();
+	for(; it.isValid(); it++) {
+		ISRIterator!(S) jt = (*it).getData().begin();
+		for(; jt.isValid(); jt++) {
+			if(!ms.contains((*it).getKey(), *jt)) {
+				return false;
+			}
+		}
+	}
+	it = ms.getMap().begin();
+	for(; it.isValid(); it++) {
+		ISRIterator!(S) jt = (*it).getData().begin();
+		for(; jt.isValid(); jt++) {
+			if(!ns.contains((*it).getKey(), *jt)) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 unittest {
 	MapSet!(int,int) ms = new MapSet!(int,int)();
 	assert(ms.insert(1,1));
+	MapSet!(int,int) ms2 = new MapSet!(int,int)();
+	assert(ms2.insert(1,1));
+	assert(Equals(ms, ms2));
+	assert(ms2.containsElement(1));
 	assert(ms.getSize() == 1);
 	assert(ms.getMapSize() == 1);
 	assert(ms.getSetSize(1) == 1);
 	assert(ms.getSetSize(0) == 0);
 	assert(ms.contains(1,1));
 	assert(!ms.contains(1,2));
+	assert(ms.containsElement(1));
 	assert(ms.insert(2,1));
 	assert(ms.insert(2,2));
+	assert(ms.containsElement(2));
 	assert(ms.insert(1,2));
 	assert(!ms.insert(1,2));
 	assert(ms.remove(1));
 	assert(!ms.remove(1));
 	assert(ms.remove(2,1));
 	assert(!ms.remove(2,1));
+
+	int[] rand = [ 123, 3, 1, 44, 2, 24, 54, 23, 67, 77, 34 ];
+	int[][] data = new int[][4];
+	data[0] = [0,1,2,3,4,5,6,7,8,9];
+	data[1] = [0,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+	data[2] = [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9];
+	data[3] = [2811, 1089, 3909, 3593, 1980, 2863, 676, 258, 2499, 3147,
+	3321, 3532, 3009, 1526, 2474, 1609, 518, 1451, 796, 2147, 56, 414, 3740,
+	2476, 3297, 487, 1397, 973, 2287, 2516, 543, 3784, 916, 2642, 312, 1130,
+	756, 210, 170, 3510, 987];
+
+	MapSet!(int,int)[] mss = new MapSet!(int,int)[9];
+	mss[0] = new MapSet!(int,int)(ISRType.RBTree, ISRType.RBTree);
+	mss[1] = new MapSet!(int,int)(ISRType.RBTree, ISRType.BinarySearchTree);
+	mss[2] = new MapSet!(int,int)(ISRType.RBTree, ISRType.HashTable);
+	mss[3] = new MapSet!(int,int)(ISRType.BinarySearchTree, ISRType.RBTree);
+	mss[4] = new MapSet!(int,int)(ISRType.BinarySearchTree, 
+		ISRType.BinarySearchTree);
+	mss[5] = new MapSet!(int,int)(ISRType.BinarySearchTree, 
+		ISRType.HashTable);
+	mss[6] = new MapSet!(int,int)(ISRType.HashTable, ISRType.RBTree);
+	mss[7] = new MapSet!(int,int)(ISRType.HashTable, ISRType.BinarySearchTree);
+	mss[8] = new MapSet!(int,int)(ISRType.HashTable, ISRType.HashTable);
 }
 
 void main() {
