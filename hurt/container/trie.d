20459c5e0e0c91273a76eb7ab40eb72648428b65
Robert BuRnEr Schadek <rburners@gmail.com>
2012-01-04 00:03:32 +0100

    a bit less const

--- hurt/container/trie.d
+++ hurt/container/trie.d
@@ -14,38 +14,38 @@ private class TrieNode(T,S) {
 		this.member = new Deque!(T)();
 	}
 
-	bool insert(const(Deque!(S)) path, size_t idx, T object) {
+	bool insert(Deque!(S) path, size_t idx, T object) {
 		if(idx == path.getSize()-1) { // reached the end of the path
 			this.member.pushBack(object);
 			return true;
 		} else if(idx < path.getSize()-1 && // path present
-				follow.contains(path.opIndexConst(idx))) {
-			return this.follow.find(path.opIndexConst(idx)).getData().
+				follow.contains(path[idx])) {
+			return this.follow.find(path[idx]).getData().
 				insert(path, idx+1, object);
 		} else if(idx < path.getSize()-1 && // path not present
-				!follow.contains(path.opIndexConst(idx))) {
+				!follow.contains(path[idx])) {
 			TrieNode!(T,S) node = new TrieNode!(T,S)();
-			this.follow.insert(path.opIndexConst(idx), node);
+			this.follow.insert(path[idx], node);
 			return node.insert(path, idx+1, object);
 		} else {
 			assert(false, "shouldn't be reached");
 		}
 	}
 
-	bool contains(const(Deque!(S)) path, size_t idx) {
+	bool contains(Deque!(S) path, size_t idx) {
 		if(idx == path.getSize()-1) {
 			return this.member.getSize() > 0;
 		} else {
-			return this.follow.find(path.opIndexConst(idx)).getData().
+			return this.follow.find(path[idx]).getData().
 				contains(path, idx+1);
 		}
 	}
 
-	T find(const(Deque!(S)) path, size_t idx) {
+	T find(Deque!(S) path, size_t idx) {
 		if(idx == path.getSize()-1) {
 			return this.member[0];
 		} else {
-			return this.follow.find(path.opIndexConst(idx)).getData().
+			return this.follow.find(path[idx]).getData().
 				find(path, idx+1);
 		}
 	}
@@ -71,22 +71,22 @@ class Trie(T,S) {
 		}
 	}
 
-	bool contains(const(Deque!(S)) path) {
+	bool contains(Deque!(S) path) {
 		// trie path must be at least one element long
 		assert(path.getSize() > 0);
-		if(this.follow.contains(path.opIndexConst(0))) { 
-			return this.follow.find(path.opIndexConst(0)).getData().
+		if(this.follow.contains(path[0])) { 
+			return this.follow.find(path[0]).getData().
 				contains(path, 1);
 		} else {
 			return false;
 		}
 	}
 
-	T find(const(Deque!(S)) path) {
+	T find(Deque!(S) path) {
 		// trie path must be at least one element long
 		assert(path.getSize() > 0);
-		if(this.follow.contains(path.opIndexConst(0))) { 
-			return this.follow.find(path.opIndexConst(0)).getData().
+		if(this.follow.contains(path[0])) { 
+			return this.follow.find(path[0]).getData().
 				find(path, 1);
 		} else {
 			static if(is(T : Object)) {
