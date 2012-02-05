8927bb0aebe8aff3b7d20dbb34e5d03ead8f8386
Robert BuRnEr Schadek <rburners@gmail.com>
2012-02-05 19:17:26 +0100

    slog got some disabling functionality

--- hurt/util/slog.d
+++ hurt/util/slog.d
@@ -21,12 +21,44 @@ unittest {
 	assert("" == cropFileName("hello/"), cropFileName("hello/"));
 }
 
-public void log(string File = __FILE__, int Line = __LINE__)() {
-	printfln("%s:%d ", cropFileName(File), Line);
+version(NOLOG) {
+	public void log(string File = __FILE__, int Line = __LINE__)() {
+		return;
+	}
+
+	public void log(string File = __FILE__, int Line = __LINE__)
+			(string format, ...) {
+		return;
+	}
+} else {
+	public void log(string File = __FILE__, int Line = __LINE__)() {
+		printfln("%s:%d ", cropFileName(File), Line);
+	}
+	
+	public void log(string File = __FILE__, int Line = __LINE__)
+			(string format, ...) {
+		printf("%s:%d ", cropFileName(File), Line);
+		printfln(format, _arguments, _argptr);
+	}
 }
 
-public void log(string File = __FILE__, int Line = __LINE__)
-		(string format, ...) {
-	printf("%s:%d ", cropFileName(File), Line);
-	printfln(format, _arguments, _argptr);
+version(NOWARN) {
+	public void warn(string File = __FILE__, int Line = __LINE__)() {
+		return;
+	}
+
+	public void warn(string File = __FILE__, int Line = __LINE__)
+			(string format, ...) {
+		return;
+	}
+} else {
+	public void warn(string File = __FILE__, int Line = __LINE__)() {
+		printfln("%s:%d WARNING", cropFileName(File), Line);
+	}
+
+	public void warn(string File = __FILE__, int Line = __LINE__)
+			(string format, ...) {
+		printf("%s:%d WARNING ", cropFileName(File), Line);
+		printfln(format, _arguments, _argptr);
+	}
 }
